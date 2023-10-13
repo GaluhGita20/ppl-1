bfe139fe1c73f78e9725e674149587a6c5f20d8c
gitaa.pragma <gita.pragma@gmail.com>
2023-10-14 01:38:02 +0800

    fix

--- database/migrations/2023_10_03_003735_create_stored_procedure_sqrt_manual.php
+++ database/migrations/2023_10_03_003735_create_stored_procedure_sqrt_manual.php
@@ -16,6 +16,7 @@ class CreateStoredProcedureSqrtManual extends Migration
     public function up()
     {
         DB::unprepared('
+        DROP FUNCTION IF EXISTS sqrt_root_manual(input DOUBLE PRECISION);
         CREATE OR REPLACE FUNCTION sqrt_root_manual(input DOUBLE PRECISION) RETURNS DOUBLE PRECISION AS $$
         DECLARE
             tebak_awal DOUBLE PRECISION;
@@ -28,20 +29,20 @@ class CreateStoredProcedureSqrtManual extends Migration
             tebak_awal := input / 2;
             iterasi := 0;
             epsilon := 0.000001;
-        
+
             start_time := NOW() AT TIME ZONE "UTC";
-        
+
             WHILE ABS(tebak_awal - iterasi) > epsilon LOOP
                 iterasi := tebak_awal;
                 tebak_awal := 0.5 * (tebak_awal + (input / tebak_awal));
             END LOOP;
-        
+
             end_time := NOW() AT TIME ZONE "UTC";
             duration := EXTRACT(EPOCH FROM (end_time - start_time));
-        
+
             INSERT INTO sys_history (tipe, input, output, duration, created_at, updated_at)
             VALUES ("SP SQL", input, tebak_awal, duration, NOW() AT TIME ZONE "UTC", NOW() AT TIME ZONE "UTC");
-        
+
             RETURN tebak_awal;
         END;
         $$ LANGUAGE plpgsql;
